dc6cc51e7a73f4b3cd9219cecdf3f6bf4ddc9a00
Geri <luga.geri@gmail.com>
2022-01-18 15:24:15 +0100

    Created Login page that displays all users

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login page/Login";
 
 export default function App() {
   return (
     <Routes>
-      <Route index element={<Navigate replace to={'/login'}/>}/>
-      <Route path={''} element={}/>
-
+      <Route index element={<Navigate replace to={"/login"} />} />
+      <Route path={'/login'} element={<Login />} />
+      {/* <Route path={'/logged-in'} element={<MainApp />}/> */}
+      {/* <Route path={"/logged-in/:id"} element={<Conversation />} /> */}
     </Routes>
   );
 }
--- src/Pages/Login page/Login.jsx
+++ src/Pages/Login page/Login.jsx
@@ -1,47 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Login = () => {
-    return (
-        <div className="main-wrapper login">
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then((resp) => resp.json())
+      .then((serverUsers) => (setUsers(serverUsers)));
+  }, [users]);
+
+  return (
+    <div className="main-wrapper login">
       <section className="login-section">
         <h2>Choose your user!</h2>
         <ul>
-          <li>
-            <button className="user-selection">
-              <img
-                className="avatar"
-                width="50"
-                height="50"
-                src="https://robohash.org/1"
-                alt=""
-              />
-              <h3>John Doe</h3>
-            </button>
-          </li>
-          <li>
-            <button className="user-selection">
-              <img
-                className="avatar"
-                width="50"
-                height="50"
-                src="https://robohash.org/2"
-                alt=""
-              />
-              <h3>Tin Man</h3>
-            </button>
-          </li>
-          <li>
-            <button className="user-selection">
-              <img
-                className="avatar"
-                width="50"
-                height="50"
-                src="https://robohash.org/3"
-                alt=""
-              />
-              <h3>Carl T-800</h3>
-            </button>
-          </li>
+          {users.map((user) => {
+            return (
+              <li>
+                <button className="user-selection">
+                  <img
+                    className="avatar"
+                    width="50"
+                    height="50"
+                    src={`https://robohash.org/${user.firstName}`}
+                    alt=""
+                  />
+                  <h3>{user.firstName + ' ' + user.lastName}</h3>
+                </button>
+              </li>
+            );
+          })}
+
           <li>
             <button className="user-selection">
               <h3>+ Add a new user</h3>
@@ -50,7 +39,7 @@ const Login = () => {
         </ul>
       </section>
     </div>
-    )
-}
+  );
+};
 
-export default Login
+export default Login;
